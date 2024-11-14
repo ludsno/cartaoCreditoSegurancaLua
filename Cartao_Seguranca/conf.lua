4d1d093f4c54a191e6d84f62c2b967aa4735e4ea
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2024-11-14 12:08:51 -0300

    Add files via upload

---
+++ Cartao_Seguranca/conf.lua
@@ -0,0 +1,10 @@
+return {
+    sailor = {
+        app_name = 'CartaoSeguro',
+        db_driver = 'pgsql',
+        db_user = 'seu_usuario',
+        db_pass = 'sua_senha',
+        db_database = 'cartao_seguro',
+        db_host = 'localhost'
+    }
+}
